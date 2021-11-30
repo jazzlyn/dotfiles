cfdb6189882e34ae386a08360727f0a838d331ba
Jazzlyn <jasmin@techtales.io>
2021-11-30 11:29:05 +0100

    Change: ranger fix command compress class and show hidden files true

--- ranger/commands.py
+++ ranger/commands.py
@@ -16,7 +16,6 @@ import os
 from ranger.api.commands import Command
 from ranger.core.loader import CommandLoader
 
-
 # Any class that is a subclass of "Command" will be integrated into ranger as a
 # command.  Try typing ":my_edit<ENTER>" in ranger!
 class my_edit(Command):
@@ -63,33 +62,33 @@ class my_edit(Command):
         return self._tab_directory_content()
 
     
-    # Compress files
-    class compress(Command):
-        def execute(self):
-            """ Compress marked files to current directory """
-            cwd = self.fm.thisdir
-            marked_files = cwd.get_selection()
+# Compress files
+class compress(Command):
+    def execute(self):
+        """ Compress marked files to current directory """
+        cwd = self.fm.thisdir
+        marked_files = cwd.get_selection()
 
-            if not marked_files:
-                return
+        if not marked_files:
+            return
 
-            def refresh(_):
-                cwd = self.fm.get_directory(original_path)
-                cwd.load_content()
+        def refresh(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
 
-            original_path = cwd.path
-            parts = self.line.split()
-            au_flags = parts[1:]
+        original_path = cwd.path
+        parts = self.line.split()
+        au_flags = parts[1:]
 
-            descr = "compressing files in: " + os.path.basename(parts[1])
-            obj = CommandLoader(args=['apack'] + au_flags + \
-                    [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr, read=True)
+        descr = "compressing files in: " + os.path.basename(parts[1])
+        obj = CommandLoader(args=['apack'] + au_flags + \
+                [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr, read=True)
 
-            obj.signal_bind('after', refresh)
-            self.fm.loader.add(obj)
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
 
-        def tab(self, tabnum):
-            """ Complete with current folder name """
+    def tab(self, tabnum):
+        """ Complete with current folder name """
 
-            extension = ['.zip', '.tar.gz', '.rar', '.7z']
-            return ['compress ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
+        extension = ['.zip', '.tar.gz', '.rar', '.7z']
+        return ['compress ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
