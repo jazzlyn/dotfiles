4cbff3345972b46297e5f72a52a39f6a4104e090
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2022-09-27 20:46:26 +0200

    fix(gcloud): remove python env

--- zsh/gcloud.zsh
+++ zsh/gcloud.zsh
@@ -1,5 +1,2 @@
 [[ -f /usr/bin/gcloud ]] && source /opt/google-cloud-sdk/completion.zsh.inc
 [[ -f /usr/bin/gcloud ]] && source /opt/google-cloud-sdk/path.zsh.inc
-
-# export GOOGLE CLOUD SDK python runtime
-[[ -f /usr/bin/gcloud ]] && export CLOUDSDK_PYTHON=/usr/bin/python2
