e7bfac1f15c98c9485ae653f9190a7ff5c64221d
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2022-09-27 22:53:05 +0200

    fix(gke-auth): update gke auth plugin

--- zsh/gcloud.zsh
+++ zsh/gcloud.zsh
@@ -1,2 +1,4 @@
 [[ -f /usr/bin/gcloud ]] && source /opt/google-cloud-sdk/completion.zsh.inc
 [[ -f /usr/bin/gcloud ]] && source /opt/google-cloud-sdk/path.zsh.inc
+
+[[ -f /usr/bin/gcloud ]] && export USE_GKE_GCLOUD_AUTH_PLUGIN="True"
