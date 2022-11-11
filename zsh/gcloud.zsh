2c47fb18705a6ebe1290d052a79984c545862125
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2022-11-11 09:20:53 +0100

    feat(secrets): reconfigure

--- zsh/gcloud.zsh
+++ zsh/gcloud.zsh
@@ -1,4 +1,5 @@
+export BOTO_PATH="$HOME/.secrets/gcloud/boto"
+
 [[ -f /usr/bin/gcloud ]] && source /opt/google-cloud-sdk/completion.zsh.inc
 [[ -f /usr/bin/gcloud ]] && source /opt/google-cloud-sdk/path.zsh.inc
-
 [[ -f /usr/bin/gcloud ]] && export USE_GKE_GCLOUD_AUTH_PLUGIN="True"
