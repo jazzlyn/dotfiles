c30b0ce7de0d3c46ea8653389c78bc313234afdc
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2023-07-28 21:31:35 +0200

    update dotfiles

--- zsh/gcloud.zsh
+++ zsh/gcloud.zsh
@@ -1,5 +1,5 @@
 export BOTO_PATH="$HOME/.secrets/gcloud/boto"
 
-[[ -f /usr/bin/gcloud ]] && source /opt/google-cloud-sdk/completion.zsh.inc
-[[ -f /usr/bin/gcloud ]] && source /opt/google-cloud-sdk/path.zsh.inc
+[[ -f /usr/bin/gcloud ]] && source /opt/google-cloud-cli/completion.zsh.inc
+[[ -f /usr/bin/gcloud ]] && source /opt/google-cloud-cli/path.zsh.inc
 [[ -f /usr/bin/gcloud ]] && export USE_GKE_GCLOUD_AUTH_PLUGIN="True"
