ef992806ce38615d2d23536a4000e72e6c9accf5
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2023-08-28 18:02:53 +0200

    fix config

--- zsh/pacman.zsh
+++ zsh/pacman.zsh
@@ -1,5 +1,5 @@
 alias pacman-aur="pacman -Qm"
 alias pacman-local="pacman -Qe"
 alias pacman-all="comm -23 <(pacman -Qeq | sort) <(pacman -Qgq | sort)"
-alias pacman-autoremove="sudo pacman -R $(pacman -Qdtq)"
-alias pacman-clearcache="pacman -Scc"
+alias pacman-autoremove="sudo pacman -R $(pacman -Qtdq)"
+alias pacman-clearcache="sudo pacman -Scc"
