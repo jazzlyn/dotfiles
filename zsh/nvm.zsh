3036187a76c9d247d3a4e683eb790a3f6b10823b
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2023-11-28 10:39:39 +0100

    feat(nvm): update zsh config

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -4,12 +4,23 @@ source /usr/share/nvm/init-nvm.sh
 # if nvmrc is present in project then load nvm version automatically
 autoload -U add-zsh-hook
 load-nvmrc() {
-  if [[ -f .nvmrc && -r .nvmrc ]]; then
-    nvm use --silence
-  elif [[ $(nvm version) != $(nvm version system)  ]]; then
-    # echo "Reverting to nvm system version"
-    nvm use system --silence
+  local nvmrc_path
+  nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version
+    nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$(nvm version)" ]; then
+      nvm use
+    fi
+  elif [ -n "$(PWD=$OLDPWD nvm_find_nvmrc)" ] && [ "$(nvm version)" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
   fi
 }
+
 add-zsh-hook chpwd load-nvmrc
 load-nvmrc
