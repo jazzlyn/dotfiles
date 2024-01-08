f4d08ad79a854a9c5455a94ae520bb81ac383f3b
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2024-01-08 10:12:57 +0100

    feat(git): add broom command to delete merged branches

--- zsh/terminal.zsh
+++ zsh/terminal.zsh
@@ -5,3 +5,11 @@ alias grep="grep --color=auto"
 
 # set compatible TERM before ssh
 alias ssh="TERM=xterm-256color ssh"
+
+git() {
+    if [[ $@ == "broom" ]]; then
+        command "$(command -v git)" branch --merged | grep -E -v "(^\*|master|main|development)" | xargs "$(command -v git)" branch -d
+    else
+        command "$(command -v git)" "$@"
+    fi
+}
