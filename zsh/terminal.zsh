05dba035082372083f4de8981f776543d2452200
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2024-05-23 09:18:21 +0200

    fix(git): adapt broom command for develop branches

--- zsh/terminal.zsh
+++ zsh/terminal.zsh
@@ -10,7 +10,12 @@ git() {
     if [[ $@ == "broom" ]]; then
         command "$(command -v git)" switch main
         command "$(command -v git)" pull
-        command "$(command -v git)" branch --merged | grep -E -v "(^\*|master|main|develop)" | xargs "$(command -v git)" branch -d
+        if command "$(command -v git)" rev-parse --verify develop >/dev/null 2>&1; then
+            command "$(command -v git)" switch develop
+            command "$(command -v git)" pull
+        fi
+        # delete merged branches except main, master, and develop
+        command "$(command -v git)" branch --merged | grep -E -v "(^\*|master|main|develop)" | xargs -r "$(command -v git)" branch -d
     else
         command "$(command -v git)" "$@"
     fi
