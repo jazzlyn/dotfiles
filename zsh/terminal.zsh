7fe0f54f17e4b3c331d5c4c3374f7ddeb3d0644a
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2024-07-21 21:55:57 +0200

    chore: linting

--- zsh/terminal.zsh
+++ zsh/terminal.zsh
@@ -7,16 +7,16 @@ alias grep="grep --color=auto"
 alias ssh="TERM=xterm-256color ssh"
 
 git() {
-    if [[ $@ == "broom" ]]; then
-        command "$(command -v git)" switch main
+  if [[ $@ == "broom" ]]; then
+    command "$(command -v git)" switch main
+    command "$(command -v git)" pull
+    if command "$(command -v git)" rev-parse --verify develop >/dev/null 2>&1; then
+        command "$(command -v git)" switch develop
         command "$(command -v git)" pull
-        if command "$(command -v git)" rev-parse --verify develop >/dev/null 2>&1; then
-            command "$(command -v git)" switch develop
-            command "$(command -v git)" pull
-        fi
-        # delete merged branches except main, master, and develop
-        command "$(command -v git)" branch --merged | grep -E -v "(^\*|master|main|develop)" | xargs -r "$(command -v git)" branch -d
-    else
-        command "$(command -v git)" "$@"
     fi
+    # delete merged branches except main, master, and develop
+    command "$(command -v git)" branch --merged | grep -E -v "(^\*|master|main|develop)" | xargs -r "$(command -v git)" branch -d
+  else
+    command "$(command -v git)" "$@"
+  fi
 }
