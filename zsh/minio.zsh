14713a61d49d5213f867c157f02ef2ba4001b65d
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2024-09-11 09:06:57 +0200

    feat(aws): update deprecated env vars

--- zsh/minio.zsh
+++ zsh/minio.zsh
@@ -1,4 +1,4 @@
 export AWS_REGION=$(sed -n 's/^AWS_REGION=//p' ~/.secrets/aws/minio-config)
-export AWS_S3_ENDPOINT=$(sed -n 's/^AWS_S3_ENDPOINT=//p' ~/.secrets/aws/minio-config)
+export AWS_ENDPOINT_URL_S3=$(sed -n 's/^AWS_ENDPOINT_URL_S3=//p' ~/.secrets/aws/minio-config)
 export AWS_ACCESS_KEY_ID=$(sed -n 's/^AWS_ACCESS_KEY_ID=//p' ~/.secrets/aws/minio-config)
 export AWS_SECRET_ACCESS_KEY=$(sed -n 's/^AWS_SECRET_ACCESS_KEY=//p' ~/.secrets/aws/minio-config)
