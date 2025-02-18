cc4c72d1232f148d0b212b9b754b9d77142003e7
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2025-02-18 18:15:38 +0100

    feat(init): update mise package

--- init.sh
+++ init.sh
@@ -4,27 +4,29 @@
 set -e
 
 check_command() {
-    if ! command -v "$1" &> /dev/null; then
-        echo "$1 not found, installing..."
-        return 1
-    else
-        echo "$1 already installed"
-        return 0
-    fi
+  if ! command -v "$1" &> /dev/null; then
+    echo "$1 not found, installing..."
+    return 1
+  else
+    echo "$1 already installed"
+    return 0
+  fi
 }
 
 # install yay if not present
 if ! check_command yay; then
-    # clean up existing directory if exists
-    [ -d "/tmp/yay" ] && rm -rf /tmp/yay
-    git clone https://aur.archlinux.org/yay.git /tmp/yay
-    cd /tmp/yay
-    yes | makepkg -si
-    rm -rf /tmp/yay
+  # clean up existing directory if exists
+  [ -d "/tmp/yay" ] && rm -rf /tmp/yay
+  git clone https://aur.archlinux.org/yay.git /tmp/yay
+  cd /tmp/yay
+  yes | makepkg -si
+  rm -rf /tmp/yay
 fi
 
 # install mise if not present
 if ! check_command mise; then
-    yay -Syu --needed --noconfirm mise-bin
-    eval "$(mise activate bash)"
+  yay -Syu --needed --noconfirm mise
+  eval "$(mise activate bash)"
+else
+  eval "$(mise activate bash)"
 fi
