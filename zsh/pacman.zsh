c23755dd14e183fabde47e4d37abed5ee1456b3e
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2026-05-30 21:08:14 +0200

    chore(pacman): rename pacman-aur to pacman-foreign

--- zsh/pacman.zsh
+++ zsh/pacman.zsh
@@ -1,4 +1,4 @@
-alias pacman-aur='pacman -Qm'
+alias pacman-foreign='pacman -Qm'
 alias pacman-local='pacman -Qe'
 alias pacman-all='comm -23 <(pacman -Qeq | sort) <(pacman -Qgq | sort)'
 alias pacman-autoremove='sudo pacman -R $(pacman -Qtdq)'
